70c8084af710aacd07dd75df2f2d106acc33ce80
srinivas931 <srinivasreddypakanati93@gmail.com>
2021-04-05 20:59:43 -0400

    Use ResponseEntity

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -1,5 +1,6 @@
 package com.example.restservice;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,10 +15,10 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public ResponseEntity<Greeting> greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         long id = counter.incrementAndGet();
-        System.out.println(id);
-        return new Greeting(id, String.format(template, name));
+        System.out.println(id); // Add logger - log4j
+        return ResponseEntity.ok(new Greeting(id, String.format(template, name)));
     }
 
 }

558cada5776fd7b054bb3c70298b62209c76c275
srinivas931 <srinivasreddypakanati93@gmail.com>
2021-04-05 21:08:51 -0400

    Add post controller

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -1,24 +1,26 @@
 package com.example.restservice;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@RequestMapping("/api/greeting")
 public class GreetingController {
 
     private  static final String template = "Hello, %s";
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/greeting")
+    @GetMapping
     public ResponseEntity<Greeting> greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         long id = counter.incrementAndGet();
         System.out.println(id); // Add logger - log4j
         return ResponseEntity.ok(new Greeting(id, String.format(template, name)));
     }
 
+    @PostMapping("/{greetingId}")
+    public ResponseEntity<Greeting> add(@PathVariable long greetingId) {
+        return ResponseEntity.ok(new Greeting(greetingId, "New Greeting Created"));
+    }
 }

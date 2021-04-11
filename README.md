87525ba6ee776e8da6bb097ffaed9499beb9a3c6
Srinivas <srinivasreddypakanati93@gmail.com>
2021-04-11 18:21:24 -0400

    REST (#1)
    * Add JPA / h2 dependencies; Repository class
    
    * Add employee controller with rest actions
    
    * Add hateoas support
    
    * Refactor with employee model assembler
    
    * Fix employee model assembler
    
    * Add firstname , lastname field

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pilot-rest-service
+
+1. https://spring.io/guides/gs/rest-service/#scratch
+2. https://spring.io/guides/tutorials/rest/
+
---
+++ src/main/java/com/example/restservice/EmployeeController.java
@@ -0,0 +1,85 @@
+package com.example.restservice;
+
+import com.example.restservice.exception.EmployeeNotFoundException;
+import com.example.restservice.payroll.Employee;
+import com.example.restservice.payroll.EmployeeModelAssembler;
+import com.example.restservice.payroll.repository.EmployeeRepository;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final EmployeeRepository repository;
+
+    private final EmployeeModelAssembler assembler;
+
+    EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
+        this.repository = repository;
+        this.assembler = assembler;
+    }
+
+    @GetMapping
+    public CollectionModel<EntityModel<Employee>> all() {
+        // return repository.findAll();
+        List<EntityModel<Employee>> employees = repository.findAll().stream()
+        .map(assembler::toModel)
+        .collect(Collectors.toList());
+
+        return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
+    }
+
+    @PostMapping
+    ResponseEntity<EntityModel<Employee>> newEmployee(@RequestBody Employee newEmployee) {
+
+        EntityModel<Employee> entityModel = assembler.toModel(repository.save(newEmployee));
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
+        //return repository.save(newEmployee);
+    }
+
+    @GetMapping("/{id}")
+    public EntityModel<Employee> one(@PathVariable Long id) {
+        Employee employee = repository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException(id));
+        //Link link = new Link("http://localhost:8080/spring-security-rest/api/customers/10A");
+        //return employee.add(link);
+
+        return assembler.toModel(employee);
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<EntityModel<Employee>> replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+        Employee updatedEmployee = repository.findById(id)
+                .map(employee -> {
+                    employee.setName(newEmployee.getName());
+                    employee.setRole(newEmployee.getRole());
+                    return repository.save(employee);
+                })
+                .orElseGet(() -> {
+                    newEmployee.setId(id);
+                    return repository.save(newEmployee);
+                });
+        EntityModel<Employee> entityModel = assembler.toModel(updatedEmployee);
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<?> deleteEmployee(@PathVariable Long id) {
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
